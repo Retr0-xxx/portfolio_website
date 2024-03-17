391fd56672534a5498e2497a260a77ae793ce809
Retr0-xxx <134814410+Retr0-xxx@users.noreply.github.com>
2024-03-16 23:18:54 -0700

    upadte

--- root/scripts/mainPage.js
+++ root/scripts/mainPage.js
@@ -1,16 +1,48 @@
 document.addEventListener('DOMContentLoaded', function() {
-        document.querySelector('.btnL').addEventListener('click', function() {
+        document.querySelector('.btnL_hard').addEventListener('click', function() {
         // Scroll left
         document.querySelector('.hardware-container').scrollBy({
             left: -200, // Adjust scroll amount (px)
             behavior: 'smooth'
         });
     });
-    document.querySelector('.btnR').addEventListener('click', function() {
+    document.querySelector('.btnR_hard').addEventListener('click', function() {
         // Scroll right
         document.querySelector('.hardware-container').scrollBy({
             left: 200, // Adjust scroll amount (px)
             behavior: 'smooth'
         });
     });
+    document.querySelector('.btnL_proj').addEventListener('click', function() {
+        // Scroll left
+        document.querySelector('.project-container').scrollBy({
+            left: -200, // Adjust scroll amount (px)
+            behavior: 'smooth'
+        });
+    });
+    document.querySelector('.btnR_proj').addEventListener('click', function() {
+        // Scroll right
+        document.querySelector('.project-container').scrollBy({
+            left: 200, // Adjust scroll amount (px)
+            behavior: 'smooth'
+        });
+    });
+    document.querySelector('.btnL_exp').addEventListener('click', function() {
+        // Scroll left
+        document.querySelector('.exp-container').scrollBy({
+            left: -200, // Adjust scroll amount (px)
+            behavior: 'smooth'
+        });
+    });
+    document.querySelector('.btnR_exp').addEventListener('click', function() {
+        // Scroll right
+        document.querySelector('.exp-container').scrollBy({
+            left: 200, // Adjust scroll amount (px)
+            behavior: 'smooth'
+        });
+    });
+    document.querySelectorAll('.hardware p').forEach((elem) => {
+        let innerHTML = elem.textContent.split(',').map(word => `<span>${word} </span>`).join('');
+        elem.innerHTML = innerHTML;
+      });
 });
